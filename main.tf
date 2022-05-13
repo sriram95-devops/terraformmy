07263b2283fecc8fa9778db0321bd8ab4fa23d73
sriram95-devops <70420859+sriram95-devops@users.noreply.github.com>
2022-05-13 20:55:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,20 +8,17 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
-}
-provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "terrasmdshj" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "eastus"
 }
 
 resource "azurerm_app_service_plan" "appplan" {
   name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.terrasmdshj.location
+  resource_group_name = azurerm_resource_group.terrasmdshj.name
 
   sku {
     tier = "Standard"
@@ -31,9 +28,9 @@ resource "azurerm_app_service_plan" "appplan" {
 
 resource "azurerm_app_service" "webappmy" {
   name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = azurerm_resource_group.terrasmdshj.location
+  resource_group_name = azurerm_resource_group.terrasmdshj.name
+  app_service_plan_id = azurerm_app_service_plan.appplan.id
 
   site_config {
     dotnet_framework_version = "v4.0"
